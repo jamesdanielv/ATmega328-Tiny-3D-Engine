a06c071669fc3adceef4d14c30b58ee2e2cee580
james <jamesdanielv@msn.com>
2022-06-07 19:00:55 -0700

    2x to 4x faster specific multiply 16+32bit numbers
    modified static long pMultiply(int32_t x, int32_t y) { } 
    function with a faster multiply by adding solution.
    also added a 8 bit + number only with 16bit integer answer fast multiply by add solution. estimate it is at least 4 times faster. it will be used for surface and for finding hidden surface math.

--- type.h
+++ type.h
@@ -12,16 +12,16 @@
 // typedef
 // ----------------------------------------------
 typedef struct {
-  long x, y, z;
+  int16_t x, y, z;
 } Vector3;
 
 typedef struct {
-  int x, y, z;
+  long x, y, z;
 } Vector3i;
 
 // fixed point identity matrix
 typedef struct {
-  long m[4][4] = {
+  int16_t m[4][4] = {
       {PRES,    0,    0,    0},
       {   0, PRES,    0,    0},
       {   0,    0, PRES,    0},
@@ -33,8 +33,70 @@ typedef struct {
 // functions
 // ----------------------------------------------
 // fixed point multiplication
-static long pMultiply(long x, long y) {
-  return ( (x * y) + PROUNDBIT) >> PSHIFT;
+static long pMultiply(int32_t x, int32_t y) {
+ 
+//return ( (x * y) + PROUNDBIT) >> PSHIFT;
+bool NEG_SIGN;
+if (x>y){int32_t temp=x;x=y;y=temp;}//we push lower value to x
+if (x<0 & y>0){NEG_SIGN=true;}//if both negative it is posative, so we only want neg if x is negative.
+if (x>0 & y<0){NEG_SIGN=true;}//if both negative it is posative
+uint32_t total=0;
+uint32_t accum=y;
+if (x<1<<15){
+uint16_t temp_x;//we make unsigned for now..
+}
+else{
+uint32_t temp_x;//we make unsigned for now..  
+}
+if (x&0b00000000000000000000000000000001){total=total+accum;};accum=accum+accum;//we double
+if (x&0b00000000000000000000000000000010){total=total+accum;};accum=accum+accum;//we double
+if (x&0b00000000000000000000000000000100){total=total+accum;};accum=accum+accum;//we double
+if (x&0b00000000000000000000000000001000){total=total+accum;};accum=accum+accum;//we double
+if (x&0b00000000000000000000000000010000){total=total+accum;};accum=accum+accum;//we double
+if (x&0b00000000000000000000000000100000){total=total+accum;};accum=accum+accum;//we double
+if (x&0b00000000000000000000000001000000){total=total+accum;};accum=accum+accum;//we double
+if (x&0b00000000000000000000000010000000){total=total+accum;};accum=accum+accum;//we double
+if (x&0b00000000000000000000000100000000){total=total+accum;};accum=accum+accum;//we double
+if (x&0b00000000000000000000001000000000){total=total+accum;};accum=accum+accum;//we double
+if (x&0b00000000000000000000010000000000){total=total+accum;};accum=accum+accum;//we double
+if (x&0b00000000000000000000100000000000){total=total+accum;};accum=accum+accum;//we double
+if (x&0b00000000000000000001000000000000){total=total+accum;};accum=accum+accum;//we double
+if (x&0b00000000000000000010000000000000){total=total+accum;};accum=accum+accum;//we double
+if (x&0b00000000000000000100000000000000){total=total+accum;};accum=accum+accum;//we double
+if (x&0b00000000000000001000000000000000){total=total+accum;};accum=accum+accum;//we double
+if( x> 1<<15){
+if (x&0b00000000000000010000000000000000){total=total+accum;};accum=accum+accum;//we double
+if (x&0b00000000000000100000000000000000){total=total+accum;};accum=accum+accum;//we double
+if (x&0b00000000000001000000000000000000){total=total+accum;};accum=accum+accum;//we double
+if (x&0b00000000000010000000000000000000){total=total+accum;};accum=accum+accum;//we double
+if (x&0b00000000000100000000000000000000){total=total+accum;};accum=accum+accum;//we double
+if (x&0b00000000001000000000000000000000){total=total+accum;};accum=accum+accum;//we double
+if (x&0b00000000010000000000000000000000){total=total+accum;};accum=accum+accum;//we double
+if (x&0b00000000100000000000000000000000){total=total+accum;};accum=accum+accum;//we double
+if (x&0b00000001000000000000000000000000){total=total+accum;};accum=accum+accum;//we double
+if (x&0b00000010000000000000000000000000){total=total+accum;};accum=accum+accum;//we double
+if (x&0b00000100000000000000000000000000){total=total+accum;};accum=accum+accum;//we double
+if (x&0b00001000000000000000000000000000){total=total+accum;};accum=accum+accum;//we double
+if (x&0b00010000000000000000000000000000){total=total+accum;};accum=accum+accum;//we double
+if (x&0b00100000000000000000000000000000){total=total+accum;};accum=accum+accum;//we double
+if (x&0b01000000000000000000000000000000){total=total+accum;};accum=accum+accum;//we double
+if (x&0b10000000000000000000000000000000){total=total+accum;};accum=accum+accum;//we double
+}
+ if (NEG_SIGN){ return -int16_t(( total + PROUNDBIT) >> PSHIFT);}else{return int16_t(( total + PROUNDBIT) >> PSHIFT);}
+}
+
+int16_t fast_8unit_multi(uint8_t x, uint8_t y){
+  uint16_t accumulator=0;
+  uint16_t solver=x;
+ if (y&0b00000001){ accumulator+=solver;};solver=solver+solver;
+ if (y&0b00000010){ accumulator+=solver;};solver=solver+solver;
+ if (y&0b00000100){ accumulator+=solver;};solver=solver+solver;
+ if (y&0b00001000){ accumulator+=solver;};solver=solver+solver;
+ if (y&0b00010000){ accumulator+=solver;};solver=solver+solver;
+ if (y&0b00100000){ accumulator+=solver;};solver=solver+solver;
+ if (y&0b01000000){ accumulator+=solver;};solver=solver+solver;
+ if (y&0b10000000){ accumulator+=solver;};
+ return accumulator;
 }
 
 #endif // TYPE_H
